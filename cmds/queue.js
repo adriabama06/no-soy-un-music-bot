dd43081c5299e6cc6e880b1060519a6d70422100
adriabama06 <adriabama06@gmail.com>
2021-09-07 22:48:03 +0200

    finally work search.js and play and queue :D

--- cmds/queue.js
+++ cmds/queue.js
@@ -13,7 +13,7 @@ module.exports = {
     /**
      * @param {{client: Discord.Client, message: Discord.Message, args: string[], prefix: string, commands: Map<string, {name: string, description: string, alias: string[], run: () => void}>, alias: Map<string, {name: string, description: string, alias: string[], run: () => void}>, Mysql: MysqlIntermediator, server, servers: Map<string, ServerManager>}} param0
      */
-    run: async ({message, servers, prefix, args}) => {
+    run: async ({client, message, servers, prefix, args, server}) => {
         const Music = servers.get(message.guild.id);
         if(!message.member.voice.channel) {
             const embed = new Discord.MessageEmbed();
@@ -45,7 +45,7 @@ module.exports = {
                 const msg = await message.channel.send({
                     embeds: [embed]
                 });
-                waitReaction(msg, "❌", message.author.id, true, true, 15000);
+                await waitReaction(msg, "❌", message.author.id, true, true, 15000);
             } else {
                 Music.songs.push(video);
                 const embed = new Discord.MessageEmbed();
@@ -55,10 +55,95 @@ module.exports = {
                 const msg = await message.channel.send({
                     embeds: [embed]
                 });
-                waitReaction(msg, "❌", message.author.id, true, true, 15000);
+                await waitReaction(msg, "❌", message.author.id, true, true, 15000);
             }
+            return;
         }
-        // Menu de la queue ...
+        var textos = [];
+        for(var i = 0; i < Music.songs.length; i++) {
+            textos.push(`
+            **[${i+1}]** -> [${Music.songs[i].videoDetails.title}](${Music.songs[i].videoDetails.video_url})`);
+        }
+
+        var menu = [];
+        var capazidad = 10;
+
+        for(var i = 0; i < textos.length; i+=capazidad) {
+            var toadd = ``;
+            var ii = 0;
+            while(ii < capazidad) {
+                if(textos[i+ii]) {
+                    toadd += textos[i+ii];
+                }
+                ii++;
+            }
+            menu.push(toadd);
+        }
+
+        const embed = new Discord.MessageEmbed();
+        embed.setTitle(`Queue : ${message.guild.name}`);
+        embed.setDescription(menu.length === 0 ? 'no hay ninguna cancion' : menu[0]);
+        embed.setTimestamp();
+        embed.setColor("RANDOM");
+        var msg = await message.channel.send({
+            embeds: [embed],
+        });
+        if(Music.songs.length > capazidad) {
+            await msg.react('⬅');
+            await msg.react('➡');
+            await msg.react('❌');
+            const filter = (reaction, user) => {
+                return ['⬅', '➡', '❌'].includes(reaction.emoji.name) && !user.bot && user.id != client.user.id;
+            }
+            var index = 0;
+            const collector = msg.createReactionCollector({filter: filter, time: 4 * 1000 * 60 });
+            collector.on('collect', async (reaction, user) => {
+                await reaction.users.remove(user.id);
+                if(reaction.emoji.name === '❌') {
+                    collector.stop();
+                    return;
+                }
+                if(reaction.emoji.name === '⬅') {
+                    if(index === 0 || index < 0) {
+                        index = menu.length-1;
+                        embed.setTitle(`${index+1} - queue : ${message.guild.name}`);
+                        embed.setDescription(menu[index]);
+                        await msg.edit({
+                            embeds: [embed]
+                        });
+                    } else {
+                        index--;
+                        embed.setTitle(`${index+1} - queue : ${message.guild.name}`);
+                        embed.setDescription(menu[index]);
+                        await msg.edit({
+                            embeds: [embed]
+                        });
+                    }
+                } else if(reaction.emoji.name === '➡') {
+                    if(index === menu.length-1 || index > menu.length-1) {
+                        index = 0;
+                        embed.setTitle(`${index+1} - queue : ${message.guild.name}`);
+                        embed.setDescription(menu[index]);
+                        await msg.edit({
+                            embeds: [embed]
+                        });
+                    } else {
+                        index++;
+                        embed.setTitle(`${index+1} - queue : ${message.guild.name}`);
+                        embed.setDescription(menu[index]);
+                        await msg.edit({
+                            embeds: [embed]
+                        });
+                    }
+                }
+            });
+            collector.on('end', async () => {
+                await msg.reactions.removeAll();
+                await msg.delete({ timeout: 5000 });
+            });
+        } else {
+            await waitReaction(msg, '❌', message.author.id, true, true, 4 * 1000 * 60);
+        }
         return;
     }
 }
--- search.js
+++ search.js
@@ -1,5 +1,5 @@
 const config = require('./config.json');
-
+const request = require('request');
 /**
  * @see 
  * @returns {Promise<string | false>}
@@ -7,23 +7,35 @@ const config = require('./config.json');
  * @param {string | number} safesearch 
  */
 module.exports = async (title, safesearch = "1") => {
-    var s;
-    if(typeof safesearch === 'number') {
-        safesearch = safesearch.toString();
-    }
-    if(safesearch === "0") {
-        s = "none";
-    }
-    if(safesearch === "1") {
-        s = "moderate";
-    }
-    if(safesearch === "2") {
-        s = "strict";
-    }
-    const res = await fetch(`https://www.googleapis.com/youtube/v3/search?q=${title}&safeSearch=${s}&type=video&key=${config.youtube.token}`, { method: 'GET' });
-    const data = await res.json();
-    if(data.items && data.items[0]) {
-        return data.items[0];
-    }
-    return false;
+    return new Promise(async (r) => {
+        var s;
+        if(typeof safesearch === 'number') {
+            safesearch = safesearch.toString();
+        }
+        if(safesearch === "0") {
+            s = "none";
+        }
+        if(safesearch === "1") {
+            s = "moderate";
+        }
+        if(safesearch === "2") {
+            s = "strict";
+        }
+        request({
+            url: `https://www.googleapis.com/youtube/v3/search?q=${title}&safeSearch=${s}&type=video&key=${config.youtube.token}`,
+            json: true
+        }, async (err, res, body) => {
+            if(err) {
+                console.log(err);
+                r(false);
+                return;
+            };
+            if(!body.items[0]) {
+                r(false);
+                return;
+            };
+            r("https://www.youtube.com/watch?v=" + body.items[0].id.videoId);
+            return;
+        });
+    });
 }
