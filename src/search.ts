dad134de3da22e17630ae6abb888b03be844e1ec
adriabama06 <adriabama06@gmail.com>
2022-01-27 16:23:46 +0100

    add search system whiout youtube token

--- src/search.ts
+++ src/search.ts
@@ -1,5 +1,6 @@
 import request from 'request';
 import config from './config';
+import * as yt from 'youtube-search-without-api-key';
 
 var search: (title: string, safesearch: string) => Promise<string | undefined>;
 
@@ -37,32 +38,12 @@ if(config.youtube.searchType === 'youtube/v3') {
 } else {
     search = async function(title: string, safesearch: string = '0'): Promise<string | undefined> {
         return new Promise(async (resolve) => {
-            var s = 'none';
-            if(safesearch === '0') {
-                s = 'none';
+            var video = await yt.search(title);
+            if(!video || !video[0]) {
+                resolve(undefined);
+                return
             }
-            if(safesearch === '1') {
-                s = 'moderate';
-            }
-            if(safesearch === '2') {
-                s = 'strict';
-            }
-            request({
-                url: `https://www.googleapis.com/youtube/v3/search?q=${title}&safeSearch=${s}&type=video&key=${config.youtube.token}`,
-                json: true
-            }, async (err, res, body) => {
-                if(err) {
-                    console.log(err);
-                    resolve(undefined);
-                    return;
-                }
-                if(!body.items || !body.items[0]) {
-                    resolve(undefined);
-                    return;
-                }
-                resolve("https://www.youtube.com/watch?v=" + body.items[0].id.videoId);
-                return;
-            });
+            resolve("https://www.youtube.com/watch?v=" + video[0].id.videoId);
         });
     }
 }
